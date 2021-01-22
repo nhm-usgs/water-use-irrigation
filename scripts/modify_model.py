17a4683d3c81cb8d815b0476c1e376612541e6e4
mrush-usgs <mrush@usgs.gov>
2021-01-22 11:37:51 -0600

    script updated for two param files

--- scripts/modify_model.py
+++ scripts/modify_model.py
@@ -1,28 +1,36 @@
-from gsflow import GsflowModel
+import gsflow 
 import numpy as np
 
-control_file = r"../yampa/control.default.bandit"
-gs = GsflowModel.load_from_file(control_file=control_file)
+
+model_dir = '../yampa/'
+control_file = model_dir+'control.default.bandit'
+gs = gsflow.GsflowModel.load_from_file(control_file=control_file)
 ctl = gs.control 
 par = gs.prms.parameters
+ag_par = gsflow.prms.PrmsParameters(parameters_list=[]) 
 
 # ag_frac, which is dynamic 
 ctl.add_record(name='dyn_ag_frac_flag', values=[1])
 ctl.add_record(name='ag_frac_dynamic', values=['dyn_ag_frac.param'])
 
+
+base_filename = './input/yampa.param'
+for params in par.record_names:
+    new_record = par.get_record(params)
+    new_record.file_name = model_dir+base_filename
+
 # parameters, which are not dynamic 
 ag_list = ['ag_soil_moist_max', 'ag_soil_moist_init_frac', 'ag_soil_rechr_max_frac', 'ag_soil_rechr_init_frac', 'sro_to_dprst_ag']
 orig_list = ['soil_moist_max', 'soil_moist_init_frac', 'soil_rechr_max_frac', 'soil_rechr_init_frac', 'sro_to_dprst_perv']
+ag_filename = './input/ag.yampa.param'
 for ind, ag_var in enumerate(ag_list): 
     if ag_var not in par.record_names:
         par_orig = par.get_record(orig_list[ind])
-        par.add_record(name=ag_var, values=par_orig._values, dimensions=par_orig._dimensions, file_name=par_orig.file_name)
+        ag_par.add_record(name=ag_var, values=par_orig._values, dimensions=par_orig._dimensions, file_name=model_dir+ag_filename)
 
-new_name = '../yampa/yampa.param'
-for params in par.record_names: 
-    new_record = par.get_record(params)
-    new_record.file_name = new_name 
-
-par.write() 
-ctl.write(name='../yampa/yampa.control')
+par_control_rec = ctl.get_record(name='param_file')
+ctl.set_values(name='param_file', values=[base_filename, ag_filename]) 
+par.write()
+ag_par.write()
+ctl.write(name=model_dir+'yampa.control')
 
